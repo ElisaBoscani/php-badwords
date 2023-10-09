c5f5b6a02237ea2590392ba90c97a733bc6c5362
Elisa Boscani <elisabosc90@gmail.com>
2023-10-09 15:36:04 +0200

    mando a schemo il contenuto del form

---
+++ index.php
@@ -0,0 +1,20 @@
+<?php 
+var_dump($_GET)
+?>
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Badwords</title>
+</head>
+<body>
+  <h1>Barwords</h1>
+  <form action="script.php" method="$_GET">
+    <input type="text" name="paragrafo" id="paragrafo" >
+    <button type="submit">invia</button>
+  </form>
+</body>
+</html>
